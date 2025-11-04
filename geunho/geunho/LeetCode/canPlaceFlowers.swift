88499a6a4ff666ac5fe9e793ac11150349fb190d
EllGHT <rmsgh1188@gmail.com>
2025-11-04 15:09:54 +0900

    canPlaceFlowers 모범답안

--- geunho/geunho/LeetCode/canPlaceFlowers.swift
+++ geunho/geunho/LeetCode/canPlaceFlowers.swift
@@ -3,7 +3,7 @@
 //  geunho
 //
 //  Created by 정근호 on 11/4/25.
-//
+//  https://leetcode.com/problems/can-place-flowers/?envType=study-plan-v2&envId=leetcode-75
 
 import Foundation
 
@@ -37,4 +37,25 @@ func canPlaceFlowers(_ flowerbed: [Int], _ n: Int) -> Bool {
     }
     
     return n <= 0 ? true : false
+}
+
+// MARK: - 모범답안
+func canPlaceFlowers1(_ flowerbed: [Int], _ n: Int) -> Bool {
+    var count = 0
+    var flowerbed = flowerbed
+    for i in 0..<flowerbed.count {
+        if flowerbed[i] == 0 {
+            let isEmptyLeft = (i == 0) || (flowerbed[i-1] == 0)
+            let isEmptyRight = (i == flowerbed.count-1) || (flowerbed[i+1] == 0)
+            
+            if isEmptyLeft && isEmptyRight {
+                flowerbed[i] = 1
+                count += 1
+                if count >= n {
+                    return true
+                }
+            }
+        }
+    }
+    return count >= n
 }
--- geunho/geunho/main.swift
+++ geunho/geunho/main.swift
@@ -15,3 +15,4 @@ import Foundation
 //print(kidsWithCandies([2,8,7], 1))
 
 print(canPlaceFlowers([0,0,1,0,0], 1))
+print(canPlaceFlowers1([0,0,1,0,0], 1))
